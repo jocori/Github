00cd0a24f6a5ffef4147714b84a2597a1d6fc745
jocori <jocori@ku.edu>
2024-10-24 16:41:44 -0500

    removed slavery columns

--- WafflingAround/waffle.R
+++ WafflingAround/waffle.R
@@ -1,3 +1,9 @@
 setwd("~/Desktop/KU/Github/WafflingAround")
-waffle<-read.csv("WaffleDivorce.csv", header = TRUE)
+
+
+library(readr) # to read in data
+waffle <- read_delim("WaffleDivorce.csv", 
+                            delim = ";", escape_double = FALSE, trim_ws = TRUE)
 head(waffle)
+## remove slavery columns :(
+waffle <- waffle[,-c(11,13)]
