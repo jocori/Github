1fb2f02d137d85c4a1419810a47e2b5287f5ba99
jocori <jocori@ku.edu>
2024-11-07 15:17:17 -0600

    added comments

--- WafflingAround/code/waffle.R
+++ WafflingAround/code/waffle.R
@@ -1,9 +1,8 @@
-setwd("~/Desktop/KU/Github/WafflingAround")
-
-
 library(readr) # to read in data
-waffle <- read_delim("WaffleDivorce.csv", 
-                            delim = ";", escape_double = FALSE, trim_ws = TRUE)
+library(here) # so all collaborators can easily get data
+
+waffle <- read_delim(here("data", "WaffleDivorce.csv"), 
+                     delim = ";", escape_double = FALSE, trim_ws = TRUE)
 head(waffle)
 ## remove slavery columns :(
 waffle <- waffle[,-c(11,13)]
