3fa6fc992a065ba5e13918446c212e6c6104c935
jocori <jocori@ku.edu>
2024-11-07 15:14:56 -0600

    moved file

--- WafflingAround/waffle.R
+++
@@ -1,9 +0,0 @@
-setwd("~/Desktop/KU/Github/WafflingAround")
-
-
-library(readr) # to read in data
-waffle <- read_delim("WaffleDivorce.csv", 
-                            delim = ";", escape_double = FALSE, trim_ws = TRUE)
-head(waffle)
-## remove slavery columns :(
-waffle <- waffle[,-c(11,13)]
