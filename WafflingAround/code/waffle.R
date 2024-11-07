eea6b1c0da553c56918b0c0ea4e51aa5f3999adf
jocori <jocori@ku.edu>
2024-11-07 15:43:45 -0600

    trying to cause conflict

--- WafflingAround/code/waffle.R
+++ WafflingAround/code/waffle.R
@@ -3,6 +3,7 @@ library(here) # so all collaborators can easily get data
 
 waffle <- read_delim(here("data", "WaffleDivorce.csv"), 
                      delim = ";", escape_double = FALSE, trim_ws = TRUE)
-head(waffle)
+head(waffle) #I love conflict
 ## remove slavery columns :(
 waffle <- waffle[,-c(11,13)]
+
